3b9b607f0d7d192c001d616bf1e5dac5b08aa244
Mikhail <mixa.bahrov@mail.ru>
2025-05-05 14:48:18 +0300

    Правки верстки
    1) Адаптирован заголовок в intro-блоке для разрешения 460px, вместо 400px
    2) Заменен логотип в бургер-меню, добавлен overlay
    3) Добавлен autoplay в "Растим чемпионов"
    4) Отредактированы стили
    5) Заменены биографии тренеров

--- services/burger_menu.js
+++ services/burger_menu.js
@@ -2,20 +2,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger_menu');
     const burger_btn = document.querySelector('.burger_btn');
     const burger_close = document.querySelector('.burger_close');
+    const burger_overlay = document.querySelector('.burger_overlay');
 
     function checkScreenSize() {
         if (window.innerWidth > 870) {
             burgerMenu.classList.remove('active');
+            burger_overlay.classList.remove('active');
         }
     }
     burger_btn.addEventListener('click', function() {
-        burgerMenu.classList.add('active')
+        burgerMenu.classList.add('active');
+        burger_overlay.classList.add('active');
     });
     burger_close.addEventListener('click', function() {
-        burgerMenu.classList.remove('active')
+        burgerMenu.classList.remove('active');
+        burger_overlay.classList.remove('active');
+    })
+    burger_overlay.addEventListener('click', function() {
+        burgerMenu.classList.remove('active');
+        burger_overlay.classList.remove('active');
     })
     document.querySelectorAll('.burger_nav_menu ul li a').forEach((el) => {
-        el.addEventListener('click', () => {burgerMenu.classList.remove('active')})
+        el.addEventListener('click', () => {
+            burgerMenu.classList.remove('active');
+            burger_overlay.classList.remove('active');
+        })
     })
     window.addEventListener('resize', checkScreenSize);
     checkScreenSize()
